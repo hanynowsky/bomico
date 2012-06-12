0ace74a130811e952aaa3719fa8f03e0f8f28c56
hanine <hanine@L502X>
2012-06-12 02:03:17 +0200

    -

--- src/bim/package-info.java
+++ src/bim/package-info.java
@@ -2,7 +2,7 @@
  * Provides the classes necessary to compute 
  * the BMI and the Graphical user Interface.
  * 
- * <p>
+ * <p>BOMICO</p>
  * The Bim class displays the GUI
  * {@link bim.Bim} class) 
  *
