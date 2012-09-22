a7f182826b71576425c1890289d08a962aa7bdab
hanine <hanine@L502X>
2012-09-22 18:27:46 +0100

    GTK Jar

--- src/com/otika/gtk/GtkGUI.java
+++ src/com/otika/gtk/GtkGUI.java
@@ -1,10 +1,17 @@
 
 package com.otika.gtk;
 
+import java.io.*;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import org.gnome.gdk.Event;
+import org.gnome.gdk.Pixbuf;
+import org.gnome.gtk.*;
+
 /**
  *
  * @author <a href="mailto:hanynowsky@gmail.com">Hanynowsky - OTIKA</a>
  */
-public class GtkGUI {
+public class GtkGUI extends Window {
     
 }
