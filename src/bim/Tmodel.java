97593173d8aa64949077e020d23dd19348e7301a
hanine <hanine@LTSL502X>
2012-09-25 12:00:01 +0100

    GTK class addition and version 1.1 alpha

--- src/bim/Tmodel.java
+++ src/bim/Tmodel.java
@@ -1,7 +1,4 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package bim;
 
 import java.io.File;
--- src/com/otika/gtk/GtkGUI.java
+++ src/com/otika/gtk/GtkGUI.java
@@ -1,4 +1,3 @@
-
 package com.otika.gtk;
 
 import java.io.*;
@@ -13,5 +12,56 @@ import org.gnome.gtk.*;
  * @author <a href="mailto:hanynowsky@gmail.com">Hanynowsky - OTIKA</a>
  */
 public class GtkGUI extends Window {
-    
+
+    // Declarations
+    Window window;
+    VBox vbox, vbox2, vbox3;
+    HBox hbox, hbox2, hbox3, hbox4, hbox5, hbox6;
+    ToolButton exitTB, computeTB, helpTB;
+    Button computeBut;
+    Toolbar toolbar;
+    MenuBar menubar;
+    Menu fileMenu, viewMenu, helpMenu;
+    MenuItem fullsMI;
+    VSeparator separator;
+    HScale weightScale, heightScale, ageScale;
+    Label resultL, transL;
+    AboutDialog about;
+    Pixbuf iconWindow, icon2, icon3, icon4;
+
+    public GtkGUI() {
+        try {
+            // Widget Creation
+            iconWindow = new Pixbuf(this.getClass().getResource("/resource/pingu.png").getFile(), 48, 48, true);
+        } catch (FileNotFoundException ex) {
+            Logger.getLogger(GtkGUI.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        window = new Window();
+        window.setDefaultSize(600, 400);
+        window.setIcon(iconWindow);
+        window.setTitle("Body Mass Index Computer - Bomico");
+        window.showAll();
+        window.connect(new Window.DeleteEvent() {
+            @Override
+            public boolean onDeleteEvent(Widget source, Event event) {
+                Gtk.mainQuit();
+                return false;
+            }
+        });
+
+        menubar = new MenuBar();
+        
+
+    } // End of Constructor
+
+    public static void main(String[] args) {
+        Gtk.init(args);
+        GtkGUI gui = new GtkGUI();
+        gui.showme();
+
+    }
+
+    private void showme() {
+        Gtk.main();
+    }
 }
